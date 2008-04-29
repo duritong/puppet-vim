5302144463847a96d4913d2776088b9ee1809f98
mh <mh@immerda.ch>
2008-04-29 09:27:25 +0000

    refactored classes, added vim-minimal for centos

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,25 +1,42 @@
 # modules/vim/manifests/init.pp - manage vim stuff
 # Copyright (C) 2007 admin@immerda.ch
+# GPLv3
 #
 
 #modules_dir { "vim": }
 
 class vim {
+    case $operatingsystem {
+        openbsd: { include vim::opensbd }
+        gentoo: { include vim::gentoo }
+        centos: { include vim::centos }
+        default: { include vim::base }
+    } 
+}
 
-        package { 'vim':
-                name => $operatingsystem ? {
-                        centos => vim-enhanced,
-                        default => vim,
-                },
-                category => $operatingsystem ? {
-                        gentoo => 'app-editors',
-                        default => '',
-                },
-                ensure => present,
-		source => $operatingsystem ? {
-			openbsd => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/vim-7.1.33-no_x11.tgz',
-			default => undef,
-		},
-        }
+class vim::base {
+    package { 'vim':
+        ensure => present,
+    }    
+}
+
+class vim::centos inherits vim::base {
+    Package[vim]{
+        name => vim-enhanced,
+    }
+    Package[vim-minimal]{
+        ensure => present,
+    }
+}
+class vim::gentoo inherits vim::base {
+    Package[vim]{
+        category => 'app-editors',
+    }
+}
+
+class vim::openbsd inherits vim::base {
+    Package[vim]{
+		source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/vim-7.1.33-no_x11.tgz',
+    }
 }
 
