98536d0de9c3f59d8df6ac9e55b7365686586164
mh <mh@immerda.ch>
2010-05-22 15:04:34 +0200

    add new latest vim release

--- manifests/openbsd.pp
+++ manifests/openbsd.pp
@@ -1,3 +1,6 @@
 class vim::openbsd {
-    openbsd::special_package{'vim-7.1.244p0-no_x11': }
+  case $operatingsystemrelease {
+    '4.3': { openbsd::special_package{'vim-7.1.244p0-no_x11': } }
+    default: { openbsd::special_package{'vim-7.2.267-no_x11.tgz': } }
+  }
 }
